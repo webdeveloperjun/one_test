504de481185dfe6664dcdfa9ee8daa93acb60315
Maksym Lesko <webdevelopereveryday@gmail.com>
2026-04-23 19:51:32 +0200

    Робота з HTTP, завдання Gemeni

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import requests
 
-response = requests.get("https://jsonplaceholder.typicode.com/posts")
-element = response.json()
-first_five = element[:5]
-for i in first_five:
-    print(i["title"])
-
-response_dict = {
-    "title": "Task completed",
-    "body": "I have notebook its cool",
-    "userId": 2344212
+keys = {
+    "password": "qdasda",
+    "user_id_phone": "123131452",
+    "X-API-KEY": "1eads132rsa"
 }
 
-new_user = requests.post("https://jsonplaceholder.typicode.com/posts", json=response_dict)
-print(new_user.status_code)
+response = requests.get("https://httpbin.org/headers", headers=keys)
+print(response.json())
